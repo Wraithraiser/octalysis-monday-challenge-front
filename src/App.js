a1e2e69fb8b4abeeba03eb49d5e7d7714f90c047
Alexandre Lim <alexandre.lim94@gmail.com>
2021-05-19 10:23:35 +0200

    feat(messages): get messages from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function getApiUri(forceProduction = false) {
+  if (forceProduction === true) {
+    return `https://octalysis-proxy-server-svu2hashgq-ew.a.run.app`;
+  }
+  return process.env.NODE_ENV !== "production"
+    ? `http://localhost:3030`
+    : `https://octalysis-proxy-server-svu2hashgq-ew.a.run.app`;
+}
+
+async function fetchMessages(setMessages) {
+  const url = `${getApiUri()}/api/messages?year=2021&month=05&day=02`;
+  try {
+    const response = await fetch(url, { method: "GET" });
+    if (response.ok) {
+      const messages = await response.json();
+      console.log("messages: ", messages);
+      setMessages(messages);
+    } else {
+      console.log("response: ", response);
+    }
+  } catch (err) {
+    console.error("Error on fetch: ", err);
+  }
+}
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    fetchMessages(setMessages);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +49,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        {messages.map((message) => (
+          <p key={message.ts}>{message.text}</p>
+        ))}
+      </div>
     </div>
   );
 }
