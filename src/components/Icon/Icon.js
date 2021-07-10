6f82e4e89602e87c85a1f8a0f0ae8a89a7f31e7d
Alexandre Lim <alexandre.lim94@gmail.com>
2021-07-10 11:58:54 +0200

    feat(header): add mobile header with menu

---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { Menu, X } from 'react-feather';
+
+const icons = {
+  menu: Menu,
+  close: X,
+};
+
+const Icon = ({ id, color, size, strokeWidth, ...delegated }) => {
+  const Component = icons[id];
+
+  if (!Component) {
+    throw new Error(`No icon found for ID: ${id}`);
+  }
+
+  return (
+    <Wrapper strokeWidth={strokeWidth} {...delegated}>
+      <Component color={color} size={size} />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  & > svg {
+    display: block;
+    stroke-width: ${(props) => (props.strokeWidth !== undefined ? props.strokeWidth + 'px' : undefined)};
+  }
+`;
+
+export default Icon;
